7763f378a6eb68b025a744c516a82d728e2bdf71
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-03-25 10:36:07 +0900

    refactor(mentor): 매직 넘버 상수로 변경

--- src/main/java/com/sprint/mission/discodeit/entity/status/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/status/UserStatus.java
@@ -8,6 +8,12 @@ import lombok.Getter;
 @Getter
 public class UserStatus implements Serializable {
 
+    private static final long ONLINE_THRESHOLD_MINUTES = 5;
+    private static final long SECONDS_PER_MINUTE = 60;
+    private static final long ONLINE_THRESHOLD_SECONDS =
+        ONLINE_THRESHOLD_MINUTES * SECONDS_PER_MINUTE;
+
+
     private final UUID id;
     private final Instant createdAt;
     private UUID userId;
@@ -24,9 +30,8 @@ public class UserStatus implements Serializable {
         this.userId = userId;
     }
 
-    // 마지막 업데이트 시간 기준으로 현재 로그한 유저 판단
     public boolean isOnline() {
-        return Instant.now().minusSeconds(5 * 60).isBefore(lastSeenAt);
+        return Instant.now().minusSeconds(ONLINE_THRESHOLD_SECONDS).isBefore(lastSeenAt);
     }
 
     public void updateLastSeenAt(Instant lastSeenAt) {
