2de8671c581a05bc49df9fdfd8d6911a5c53998c
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-04-02 11:31:18 +0900

    feature: swagger ui

---
+++ src/main/java/com/sprint/mission/discodeit/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.discodeit.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("sprint5 API")
+            .description("sprint5 미션 API 문서입니다.")
+            .version("1.0,0");
+    }
+}
