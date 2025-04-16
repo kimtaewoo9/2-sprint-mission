616267f65b63c2bc263af6232fd0e836cf76a583
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-04-16 18:11:21 +0900

    feature: message mapper refactoring

--- src/main/java/com/sprint/mission/discodeit/entity/common/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/common/BaseUpdatableEntity.java
@@ -3,11 +3,13 @@ package com.sprint.mission.discodeit.entity.common;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
+import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public abstract class BaseUpdatableEntity extends BaseEntity {
 
     @LastModifiedDate
--- src/main/java/com/sprint/mission/discodeit/mapper/MessageMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/MessageMapper.java
@@ -2,12 +2,26 @@ package com.sprint.mission.discodeit.mapper;
 
 import com.sprint.mission.discodeit.dto.message.MessageDto;
 import com.sprint.mission.discodeit.entity.Message;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MessageMapper {
 
-    public MessageDto toDto(Message message) {
+    private final BinaryContentMapper binaryContentMapper;
+    private final UserMapper userMapper;
 
+    public MessageDto toDto(Message message) {
+        return new MessageDto(
+            message.getId(),
+            message.getCreatedAt(),
+            message.getUpdatedAt(),
+            message.getContent(),
+            message.getChannel().getId(),
+            userMapper.toDto(message.getAuthor()),
+            message.getAttachments().stream()
+                .map(binaryContentMapper::toDto).toList()
+        );
     }
 }
